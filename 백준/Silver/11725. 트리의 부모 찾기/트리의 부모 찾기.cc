fe19140979eb6f5bb11e8fc517ed09aa00b19bc7
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2022-07-02 23:30:52 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 72 ms, Memory: 12488 KB -BaekjoonHub

---
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n;
+vector <int> v[100001];
+int ans[100001];
+
+void dfs(int x){
+	for(int i = 0; i< v[x].size(); i++){
+		int next = v[x][i];
+		if(ans[next] == 0){
+			ans[next] = x;
+			dfs(next);
+		}
+	}
+}
+
+int main() {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL);
+  cout.tie(NULL);
+
+  cin>>n;
+
+  for(int i = 0; i<n-1; i++){
+	  int a, b;
+	  cin>>a>>b;
+
+	  v[a].push_back(b);
+	  v[b].push_back(a);
+  }
+  //cout<<"done";
+  ans[1] = 1;
+  //cout<<"start";
+  dfs(1);
+  //cout<<"finished";
+
+  for(int i = 2; i<=n; i++){
+	  cout<<ans[i]<<"\n";
+  }
+
+  return 0;
+}
