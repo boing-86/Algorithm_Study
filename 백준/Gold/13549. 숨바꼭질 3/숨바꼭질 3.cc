bea71d1f32a1a9e07dda99797b440948dfadc36f
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2024-01-28 18:36:17 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 0 ms, Memory: 2384 KB -BaekjoonHub

---
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.cc
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#include <vector>
+using namespace std;
+
+int n, k;
+bool visited[100001];
+
+void bfs(int x){
+  queue <pair<int, int>> q;
+
+  q.push({x, 0});
+
+  while(!q.empty()){
+    int next = q.front().first;
+    int distance = q.front().second;
+
+    q.pop();
+
+    if(next == k){
+      cout<<distance;
+      break;
+    }
+
+    if(next*2 >= 0 && next*2 < 100001){
+      if(!visited[next*2]){
+        visited[next*2] = true;
+        q.push({next*2, distance});
+      }
+    }
+
+    if(next-1 >= 0 && next-1 < 100001){
+      if(!visited[next-1]){
+        visited[next-1] = true;
+        q.push({next-1, distance+1});
+      }
+    }
+
+    if(next+1 >= 0 && next+1 < 100001){
+      if(!visited[next+1]){
+        visited[next+1] = true;
+        q.push({next+1, distance+1});
+      }
+    }
+  }
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL);
+
+  cin>>n>>k;
+
+  visited[n] = true;
+
+  bfs(n);
+
+  return 0;
+}
