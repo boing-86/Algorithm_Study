a4a0a84d25760164bbf19a3972afe95a9314c2ee
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2024-04-01 15:22:20 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string phone_number) {
+    string answer = "";
+    
+    for(int i = 0; i<phone_number.size() - 4; i++){
+        answer.push_back('*');
+    }
+    for(int i = phone_number.size()-4; i<phone_number.size(); i++){
+        answer.push_back(phone_number[i]);
+    }
+    return answer;
+}
