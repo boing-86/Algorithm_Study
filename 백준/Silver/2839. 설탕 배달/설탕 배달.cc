2095c499237ee0686eb377140de081ed05f0208b
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2024-05-22 14:54:39 +0900

    [Silver IV] Title: 설탕 배달, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cc
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cc
@@ -1,35 +1,32 @@
 #include <iostream>
-#include <string.h>
-
+#include <algorithm>
+#include <vector>
+#include <cmath>
 using namespace std;
 
-const int INF = 987654321;
-int cache[5001];
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
 
-int dp(int a){
-  if(a < 0){ return INF; }
-  if(a == 0){ return 0; }
-  if(cache[a] != -1){ return cache[a]; }
-  
-  cache[a] = min(dp(a-3)+1, dp(a-5)+1);
-  return cache[a];
-}
+    int n;
+    int dp[5001];
+    dp[0] = dp[1] = dp[2] = dp[4] = dp[7] = -1;
+    dp[3] = dp[5] = 1;
+    dp[6] = dp[8] = dp[10] = 2;
 
-int main() {
-  std::ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
+    dp[9] = dp[11] = 3;
+    dp[12] = 4;
 
-  int n;
-  cin>>n;
+    cin>>n;
+    if(n <= 12){
+        cout<< dp[n];
+        return 0;
+    }
 
-  memset(cache, -1, 5001*sizeof(int));
+    for(int i = 13; i<=n; i++){
+        dp[i] = min(dp[i-3] + 1, dp[i-5] + 1);
+    }
 
-  cache[3] = cache[5] = 1;
-  int result = dp(n);
-
-  if(result <= 5001){
-    cout<<result;
-  } else{
-    cout<<-1;
-  }
+    cout<<dp[n];
+    return 0;
 }
