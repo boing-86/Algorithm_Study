a43bc5948c6816135c79875df3a66917bca86036
Jeong Boin <54930076+boing-86@users.noreply.github.com>
2024-08-19 18:25:10 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <algorithm>
+#include <numeric>
+#include <cmath>
+using namespace std;
+
+
+long long recursive(long long a, long long b, long long c){
+
+    if(b == 0){
+        return 1;
+    }
+    
+    if(b == 1){
+        return a % c;
+    }
+
+    else{
+        long long r = recursive(a, b/2, c);
+
+        if(b % 2 == 0){
+            return r*r%c;
+        }
+
+        else{
+            return r*r%c*a%c;
+        }
+    }
+}
+
+int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    long long a, b, c;
+    cin>>a>>b>>c;
+
+    cout<<recursive(a, b, c);
+
+}
